470608ac91993ecf58560610f7fa4b8fed9c0088
ramvignesh-b <43937419+ramvignesh-b@users.noreply.github.com>
2026-07-18 11:25:28 +0530

    fix(setup): change to stable provideZonelessChangeDetection API

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideHttpClient(),
     provideAnimations()
   ]
