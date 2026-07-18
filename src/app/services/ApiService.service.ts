c7095cfb1ff5c22c71c3454a4af12ed6eb5eb7b7
ramvignesh-b <43937419+ramvignesh-b@users.noreply.github.com>
2026-07-18 11:10:05 +0530

    feat(services): migrate ApiService to providedIn root format

--- src/app/services/ApiService.service.ts
+++ src/app/services/ApiService.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class ApiService {
     constructor(
         private http: HttpClient
