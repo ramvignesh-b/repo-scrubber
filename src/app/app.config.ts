4aa6f6865817f69bdd7a7a48dc93e68ae87e3fd4
ramvignesh-b <43937419+ramvignesh-b@users.noreply.github.com>
2026-07-18 11:09:56 +0530

    feat(setup): bootstrap standalone application and style with Material UI

---
+++ src/app/app.config.ts
@@ -0,0 +1,11 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideAnimations()
+  ]
+};
