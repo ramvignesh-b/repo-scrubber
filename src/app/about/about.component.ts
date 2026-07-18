1cb2f98f96f0d99b1543c862b56fd5034db433d7
ramvignesh-b <43937419+ramvignesh-b@users.noreply.github.com>
2026-07-18 11:11:19 +0530

    feat(about): migrate AboutComponent to standalone and revamp style and copy

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-about',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class AboutComponent {}
