1735c5310a9e54c7e160856b2c51f2a568e6cd7b
ramvignesh-b <43937419+ramvignesh-b@users.noreply.github.com>
2026-07-18 11:25:14 +0530

    fix(setup): configure experimental zoneless change detection to fix bootstrap error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
     provideHttpClient(),
     provideAnimations()
   ]
