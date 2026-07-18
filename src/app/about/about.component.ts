6ad7f3e3507705a0ecde75603b1fe30b4741a497
ramvignesh-b <43937419+ramvignesh-b@users.noreply.github.com>
2026-07-18 11:35:24 +0530

    feat(info): migrate AboutComponent and HowToComponent to Primer style

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [
-    CommonModule,
-    MatExpansionModule,
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule
-  ],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
--- src/app/how-to/how-to.component.ts
+++ src/app/how-to/how-to.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-how-to',
   standalone: true,
-  imports: [
-    CommonModule,
-    MatStepperModule,
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule
-  ],
+  imports: [CommonModule],
   templateUrl: './how-to.component.html',
   styleUrls: ['./how-to.component.scss']
 })
