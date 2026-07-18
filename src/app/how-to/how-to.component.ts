eda64de109de65cf00513a0b1488ff66d726e396
ramvignesh-b <43937419+ramvignesh-b@users.noreply.github.com>
2026-07-18 11:11:46 +0530

    feat(howto): migrate HowToComponent to standalone and style with vertical stepper

--- src/app/how-to/how-to.component.ts
+++ src/app/how-to/how-to.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-how-to',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatStepperModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './how-to.component.html',
   styleUrls: ['./how-to.component.scss']
 })
-export class HowToComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class HowToComponent {}
