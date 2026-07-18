78b91aaa0c332f0df7f24c79d33081484ec368f9
ramvignesh-b <43937419+ramvignesh-b@users.noreply.github.com>
2026-07-18 11:10:28 +0530

    feat(loading): migrate LoadingScreenComponent to standalone and style with Material spinner

--- src/app/loading-screen/loading-screen.component.ts
+++ src/app/loading-screen/loading-screen.component.ts
@@ -1,16 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-loading-screen',
+  standalone: true,
+  imports: [CommonModule, MatProgressSpinnerModule],
   templateUrl: './loading-screen.component.html',
   styleUrls: ['./loading-screen.component.scss']
 })
-export class LoadingScreenComponent implements OnInit {
-@Input() show: boolean = false;
-@Input() message: string = '';
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class LoadingScreenComponent {
+  @Input() show: boolean = false;
+  @Input() message: string = '';
 }
